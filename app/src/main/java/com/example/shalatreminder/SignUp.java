f9ab6b843f65896167de00e12fc99ac57d062f49
brraleant <aliapute@gmail.com>
2019-04-07 16:16:12 +0700

    lol

---
+++ app/src/main/java/com/example/shalatreminder/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.shalatreminder;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUp extends Activity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.sign_up);
+
+
+    }
+}
